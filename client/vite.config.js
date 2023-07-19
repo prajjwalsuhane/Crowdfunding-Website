3a1396fcc5f7ee76a13a4a27e103c12cf859493f
Prajjwal Suhane <prajjwalsuhane123@gmail.com>
2023-07-19 19:00:17 +0530

    updating the main structure and functions of the website

---
+++ client/vite.config.js
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), nodePolyfills()],
+  define: {
+    "process.env": {},
+  },
+  resolve: {
+    
+
+    // or
+    alias: [
+      {
+        find: 'web3',
+        replacement: 'web3/dist/web3.min.js',
+      },
+    ],
+  },
+});
